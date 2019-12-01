83077172b9e98aae58062de8575ecab8b4e1aeeb
Lucas Sousa <lucassh.dev@gmail.com>
2019-12-01 15:39:25 -0300

    Template Login/Cadastro add

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :admins, path: 'admins', controllers: { 
+    sessions: 'admins/sessions',
+    registrations: 'admins/registrations'
+  }
+
+  devise_scope :admin do  
+    get '/admins/sign_out' => 'admins/sessions#destroy'     
+  end
+
+  root to: 'home#index'
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
