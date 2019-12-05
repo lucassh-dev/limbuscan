70fabeaaef88134cd8137a0b2a7433615b41e7e6
Lucas Sousa <lucassh.dev@gmail.com>
2019-12-05 14:47:28 -0300

    Template login/cadastro corrigidos

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link plugins/bootstrap/css/bootstrap.css
 //= link plugins/fontawesome-free/css/all.css
+//= link plugins/ionicons/css/ionic.css
+//= link plugins/adminlte/adminlte.css
+//= link plugins/icheck/icheck.css
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,8 @@ require("channels");
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import './adminlte';
+import './jquery-2.1.4';
+import './bootstrap';
+
+import '../../assets/stylesheets/application.scss';
+
